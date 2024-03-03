fb01483c8512571f4c01e0484d60d99b697a0c5a
Tony Lea <tnylea@gmail.com>
2024-03-03 16:53:20 -0500

    Adding latest updates

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,1 @@
+<p>Rad Sauce</p>
---
+++ src/Home/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Chronicles\Chronicles\Home\Home;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('chronicles::livewire.home');
+    }
+}
